c1cfcfaa064e03c54a54cf031ca986bd12fafea0
ganariya <ganariya2525@gmail.com>
2021-01-12 21:34:08 +0900

    バッジにロゴを追加 Pybadgesの追加

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Run FastAPI with Vercel
 
 Live demo -> https://vercelfastapi-git-master.dariomoceri.vercel.app/docs
+
+![](http://127.0.0.1:8000/ganariya/likes)
--- zenn_budge/app.py
+++ zenn_budge/app.py
@@ -1,17 +1,41 @@
+import pybadges
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 from .zenn import scrape_user
+from .user import User
+from .logo import LOGO
 
 app = FastAPI()
+BASE_COLOR: str = '#3FA8FF'
+
+
+def make_badge(left_text: str, right_text: str) -> str:
+    return pybadges.badge(left_text=left_text, right_text=right_text, right_color=BASE_COLOR, embed_logo=True, logo=LOGO)
 
 
 @app.get("/")
-def main():
-    return {
-        "message": "Hello my friend"
-    }
+def main() -> str:
+    return "Zenn Budges"
 
 
-@app.get("/test")
-def test_get():
-    scrape_user("ganariya")
-    return 0
+@app.get("/{username}/liked", response_class=HTMLResponse)
+def get_liked(username: str) -> str:
+    user: User = scrape_user(username)
+    badge = make_badge('Zenn liked', str(user.total_liked_count))
+    return badge
+
+
+@app.get("/{username}/articles", response_class=HTMLResponse)
+def get_liked(username: str) -> str:
+    user: User = scrape_user(username)
+    badge = make_badge('Zenn articles', str(user.articles_count))
+    return badge
+
+
+@app.get("/{username}/followers", response_class=HTMLResponse)
+def get_liked(username: str) -> str:
+    user: User = scrape_user(username)
+    badge = make_badge('Zenn followers', str(user.follower_count))
+    return badge
