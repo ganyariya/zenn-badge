dc44ec19b35555614e4ffff2aae19603bdc45c4c
ganariya <ganariya2525@gmail.com>
2021-01-12 21:39:37 +0900

    userへのURLを追加

--- zenn_budge/app.py
+++ zenn_budge/app.py
@@ -11,8 +11,9 @@ app = FastAPI()
 BASE_COLOR: str = '#3FA8FF'
 
 
-def make_badge(left_text: str, right_text: str) -> str:
-    return pybadges.badge(left_text=left_text, right_text=right_text, right_color=BASE_COLOR, embed_logo=True, logo=LOGO)
+def make_badge(username: str, left_text: str, right_text: str) -> str:
+    url = f'https://zenn.dev/{username}'
+    return pybadges.badge(left_text=left_text, right_text=right_text, right_color=BASE_COLOR, embed_logo=True, logo=LOGO, left_link=url, right_link=url)
 
 
 @app.get("/")
@@ -23,19 +24,19 @@ def main() -> str:
 @app.get("/{username}/liked", response_class=HTMLResponse)
 def get_liked(username: str) -> str:
     user: User = scrape_user(username)
-    badge = make_badge('Zenn liked', str(user.total_liked_count))
+    badge = make_badge(username, 'Zenn liked', str(user.total_liked_count))
     return badge
 
 
 @app.get("/{username}/articles", response_class=HTMLResponse)
 def get_liked(username: str) -> str:
     user: User = scrape_user(username)
-    badge = make_badge('Zenn articles', str(user.articles_count))
+    badge = make_badge(username, 'Zenn articles', str(user.articles_count))
     return badge
 
 
 @app.get("/{username}/followers", response_class=HTMLResponse)
 def get_liked(username: str) -> str:
     user: User = scrape_user(username)
-    badge = make_badge('Zenn followers', str(user.follower_count))
+    badge = make_badge(username, 'Zenn followers', str(user.follower_count))
     return badge
