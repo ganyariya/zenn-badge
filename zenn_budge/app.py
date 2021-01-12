aef70f1b170d61111c020025f6aa98209b4b1bbc
ganariya <ganariya2525@gmail.com>
2021-01-12 20:29:19 +0900

    simple change

---
+++ zenn_budge/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from .test import test
+
+app = FastAPI()
+
+
+@app.get("/")
+def main():
+    return {
+        "message": "Hello my friend"
+    }
+
+
+@app.get("/test")
+def test_get():
+    return test()
