845da18eb352a296f234bd9676e29773757e1b28
ganariya <ganariya2525@gmail.com>
2021-01-13 20:56:43 +0900

    simple change

--- zenn_badge/app.py
+++ zenn_badge/app.py
@@ -1,7 +1,8 @@
 import pybadges
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from .zenn import scrape_user
@@ -9,11 +10,18 @@ from .user import User
 from .badge import make_badge
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 def main() -> str:
     return "Zenn Badges"
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
 
 @app.get("/{username}/liked", response_class=HTMLResponse)
